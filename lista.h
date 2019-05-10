4dc33b81f42f67c849e852b5a377f1fec7b1e210
willy.leiser@gmail.com <willy.leiser@gmail.com>
2019-05-10 20:05:25 -0300

    comecei a fazer a lista

--- lista.h
+++ lista.h
@@ -1,3 +1,18 @@
 
 
+//cria lista e retorna o ponteiro p/ inicio da lista
+listaFigutas createLista();
+
+//retorna o tamanho da lista
+int lenght(listaFiguras list);
+
+//insere um elemento na lista e retorna um indicador (ver sobre esse indicador)
+Posic insert(listaFiguras list, Info info);
+
+//remove um elemento da lista 
+void delet(listaFiguras list, Posic p);
+
+
+
+
 
