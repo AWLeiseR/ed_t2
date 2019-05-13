7fd1b971cd2a509d59aa5c2141d160cec5310810
willy.leiser@gmail.com <willy.leiser@gmail.com>
2019-05-13 13:11:42 -0300

    progresso na lista

--- lista.c
+++ lista.c
@@ -2,6 +2,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+ 
+#define tam_lista 10000
 
 typedef struct{
     char tipo;
@@ -9,9 +11,13 @@ typedef struct{
     double h,w;
 }Retangulo;
 
+typedef struct{
+    double i;
+}Info;
+
 
 typedef struct{
-    Retangulo forma;
+    Retangulo *forma;
     int ant;
     int prox
 }Item;
@@ -21,14 +27,40 @@ typedef struct{
     int primeiro;
     int livre;
     int ultimo;
-}*ListaFiguras;
+}ListaFiguras;
 
-ListaFiguras criaLista(int tam){
-    ListaFiguras lista= malloc(sizeof(ListaFiguras));
-    lista->info = malloc(sizeof(Item)*tam);
+ListaFiguras* createLista(){
+    int i=0 ;
+    ListaFiguras* aux;
+    ListaFiguras* lista= malloc(sizeof(ListaFiguras));
+    lista->info = malloc(sizeof(Item)*tam_lista);
     lista->livre=0;
-    lista->primeiro=0;
-    lista->ultimo=tam-1;
+    lista->primeiro=-1;
+    lista->ultimo=-1;
+    aux=lista;
+    for(i=0;i<tam_lista;i++){
+        aux.info[i].prox=1+i;
+    }
+    aux.info[tam_lista-1].prox=-1;
     return lista;
+}
 
+int lenght(listaFiguras* list){
+    int i=0;
+    ListaFiguras aux;
+    aux=list->primeiro;
+    while(1){
+        if(aux.prox!=-1){
+            aux=aux.info[aux].prox;
+            i++;
+        }else{
+            break;
+        }        
+    }
+    return i;
+}
+
+int insert(listaFiguras list, Info info){
+    Info nItem;
+    list.info[list.livre]=info;
 }
--- lista.h
+++ lista.h
@@ -3,11 +3,16 @@
 //cria lista e retorna o ponteiro p/ inicio da lista
 listaFigutas createLista();
 
+//inicializa a lista
+void inicializaLista();
+
 //retorna o tamanho da lista
 int lenght(listaFiguras list);
 
 //insere um elemento na lista e retorna um indicador (ver sobre esse indicador)
-Posic insert(listaFiguras list, Info info);
+int insert(listaFiguras list, Info info);
+
+
 
 //remove um elemento da lista 
 void delet(listaFiguras list, Posic p);
